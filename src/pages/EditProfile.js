1207110cc806c5f1e88de0232e8521402e74ac9c
shuiRong <shuirong1997@icloud.com>
2019-01-14 21:15:33 +0800

    添加编辑个人资料页面；完成部分逻辑;

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -3,129 +3,38 @@
  */
 
 import React, { Component } from 'react'
-import { View, StyleSheet, FlatList, RefreshControl } from 'react-native'
-import { Spinner, Header, Left, Body, Right, Button, Title } from 'native-base'
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+  Text
+} from 'react-native'
+import {
+  Spinner,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+  Title,
+  CheckBox
+} from 'native-base'
 import { getNotifications } from '../utils/api'
 import Icon from 'react-native-vector-icons/FontAwesome5'
-import TootBox from './common/TootBox'
-import ListFooterComponent from './common/ListFooterComponent'
 import { color } from '../utils/color'
-import Divider from './common/Divider'
 
 export default class EditProfile extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      list: [],
-      loading: true
+      loading: false,
+      description: '',
+      displayName: '',
+      lockAccount: true
     }
   }
-  componentDidMount() {
-    this.getNotifications()
-  }
-
-  /**
-   * @description 检测其他页面跳转过来的动作，比如发嘟页面跳转过来时可能带有toot数据，塞入数据流中
-   * 如果带有一些参数；根据参数更新数据状态
-   */
-  componentWillReceiveProps({ navigation }) {
-    if (!navigation) {
-      return
-    }
-    const params = navigation.getParam('data')
-
-    if (params && params.id) {
-      let newList = this.state.list
-      if (params.mute) {
-        // 如果某人被‘隐藏’，那么首页去除所有该用户的消息
-        newList = newList.filter(item => item.account.id !== params.accountId)
-        this.setState({
-          list: newList
-        })
-        return
-      }
-      // 改变某条toot的点赞等状态
-      newList = newList.map(item => {
-        if (item.id !== params.id) {
-          return item
-        }
-        return {
-          ...item,
-          reblogs_count: params.reblogs_count,
-          favourites_count: params.favourites_count,
-          favourited: params.favourited,
-          reblogged: params.reblogged
-        }
-      })
-
-      this.setState({
-        list: newList
-      })
-    }
-
-    const toot = navigation.getParam('newToot')
-    if (toot) {
-      // 将新toot塞入数据最上方
-      const newList = [...this.state.list]
-      newList.unshift(toot)
-
-      this.setState({
-        list: newList
-      })
-    }
-  }
-
-  deleteToot = id => {
-    this.setState({
-      list: this.state.list.filter(toot => toot.id !== id)
-    })
-  }
-
-  // 清空列表中刚被mute的人的所有消息
-  muteAccount = id => {
-    this.setState({
-      list: this.state.list.filter(toot => toot.account.id !== id)
-    })
-  }
-
-  // 清空列表中刚被mute的人的所有消息
-  blockAccount = id => {
-    this.setState({
-      list: this.state.list.filter(toot => toot.account.id !== id)
-    })
-  }
-
-  /**
-   * @description 获取时间线数据
-   * @param {cb}: 成功后的回调函数
-   * @param {params}: 分页参数
-   */
-  getNotifications = (cb, params) => {
-    getNotifications(params).then(res => {
-      // 同时将数据更新到state数据中，刷新视图
-      this.setState({
-        list: this.state.list.concat(res),
-        loading: false
-      })
-      if (cb) cb()
-    })
-  }
-
-  refreshHandler = () => {
-    this.setState({
-      loading: true,
-      list: []
-    })
-    this.getNotifications()
-  }
-
-  // 滚动到了底部，加载数据
-  onEndReached = () => {
-    const state = this.state
-    this.getNotifications(null, {
-      max_id: state.list[state.list.length - 1].id
-    })
-  }
+  componentDidMount() {}
 
   render() {
     const state = this.state
@@ -153,31 +62,89 @@ export default class EditProfile extends Component {
             </Button>
           </Right>
         </Header>
-        <FlatList
-          ItemSeparatorComponent={() => <Divider />}
-          showsVerticalScrollIndicator={false}
-          data={state.list}
-          onEndReachedThreshold={0.1}
-          onEndReached={this.onEndReached}
-          onScroll={this.props.onScroll}
-          keyExtractor={item => item.id}
-          refreshControl={
-            <RefreshControl
-              refreshing={state.loading}
-              onRefresh={this.refreshHandler}
+        <View class={styles.body}>
+          <ImageBackground
+            source={{
+              uri:
+                'https://cmx.im/system/accounts/headers/000/081/232/original/f6d371ea63e221e1.jpeg?1544620479'
+            }}
+            style={styles.bg}
+          >
+            <View
+              style={{
+                width: '100%',
+                height: 200,
+                backgroundColor: color.moreBlack,
+                opacity: 0.5,
+                position: 'absolute'
+              }}
             />
-          }
-          ListFooterComponent={() => <ListFooterComponent />}
-          renderItem={({ item }) => (
-            <TootBox
-              data={item}
-              navigation={this.props.navigation}
-              deleteToot={this.deleteToot}
-              muteAccount={this.muteAccount}
-              blockAccount={this.blockAccount}
-            />
-          )}
-        />
+            <Icon name="cloud-upload-alt" style={styles.upload} />
+            <ImageBackground
+              source={{
+                uri:
+                  'https://cmx.im/system/accounts/avatars/000/081/232/original/6fde03bb798bd111.jpg?1544620479'
+              }}
+              style={styles.avatarBg}
+              imageStyle={{
+                borderRadius: 8
+              }}
+            >
+              <View
+                style={{
+                  width: 80,
+                  height: 80,
+                  backgroundColor: color.moreBlack,
+                  opacity: 0.5,
+                  position: 'absolute'
+                }}
+              />
+              <Icon
+                name="cloud-upload-alt"
+                style={{ ...styles.upload, fontSize: 24 }}
+              />
+            </ImageBackground>
+          </ImageBackground>
+          <View style={{ padding: 20, marginTop: 50 }}>
+            <View>
+              <Text style={{ fontSize: 17, color: color.grey }}>昵称</Text>
+              <TextInput
+                style={styles.baseInput}
+                onChangeText={text =>
+                  this.setState({
+                    displayName: text
+                  })
+                }
+                value={state.displayName}
+                maxLength={10}
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text>简介</Text>
+              <TextInput
+                style={[styles.baseInput, styles.multilineInput]}
+                onChangeText={text =>
+                  this.setState({
+                    description: text
+                  })
+                }
+                value={state.description}
+                multiline={true}
+                textAlignVertical={'top'}
+                placeholder={'...'}
+                maxLength={400}
+                numberOfLines={4}
+              />
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+              <CheckBox checked={state.lockAccount} color="green" />
+              <View>
+                <Text>保护你的账户（锁嘟）</Text>
+                <Text>你需要手动审核所有关注请求</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     )
   }
@@ -189,8 +156,44 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     backgroundColor: color.white
   },
+  body: {
+    flex: 1,
+    backgroundColor: 'lightgreen'
+  },
   navIcon: {
     fontSize: 20,
     color: color.lightGrey
+  },
+  bg: {
+    width: '100%',
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  avatarBg: {
+    width: 80,
+    height: 80,
+    position: 'absolute',
+    bottom: -40,
+    left: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  upload: {
+    color: color.white,
+    fontSize: 30
+  },
+  baseInput: {
+    borderWidth: 0,
+    borderColor: color.headerBg,
+    borderBottomWidth: 2,
+    padding: 5,
+    fontSize: 20,
+    color: color.moreBlack,
+    height: 50
+  },
+  multilineInput: {
+    height: 120,
+    backgroundColor: 'lightblue'
   }
 })
