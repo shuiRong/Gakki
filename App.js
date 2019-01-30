b8e09f92492f66b13a179e7c9bc1fa2a5cbdaa15
shuiRong <shuirong1997@icloud.com>
2019-01-30 12:41:15 +0800

    修改console设置:set error as exception;

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { createStackNavigator } from 'react-navigation'
 import Main from './src/Main'
-// // 禁用所有黄色警告
+// 禁用所有黄色警告
 console.disableYellowBox = true
+// 将报错（会在屏幕上弹出，影响操作）转换为异常（只会在控制台报错，不影响操作）
+console.reportErrorsAsExceptions = false
 
 export default createStackNavigator(
   {
